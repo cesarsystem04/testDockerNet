c548ffd574a0209a407896a347e7bba9e29f2196
cesarsystem04 <cesarsystem04@gmail.com>
2022-12-07 22:20:53 -0600

    Initial commit

---
+++ App/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace DotNet.Docker
+{
+    class Program
+    {
+       public static async Task Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+           var counter = 0;
+           var max = args.Length != 0 ? Convert.ToInt32(args[0]) : -1;
+            while (max == -1 || counter < max)
+            {
+                Console.WriteLine($"Counter: {++counter}");
+                await Task.Delay(TimeSpan.FromMilliseconds(1_000));
+            }
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# testDockerNet
+ Repositorio de prueba para crear imagen de Docker de aplicación en NetCore.
